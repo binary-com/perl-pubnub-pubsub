51a8f54a762a4ca712762924654dc31bf2c4c38a
Fayland Lam <fayland@gmail.com>
2014-08-27 14:02:07 +0800

    from milla new

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+PubNub::PubSub - Blah blah blah
+
+# SYNOPSIS
+
+    use PubNub::PubSub;
+
+# DESCRIPTION
+
+PubNub::PubSub is
+
+# AUTHOR
+
+Fayland Lam <fayland@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2014- Fayland Lam
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/PubNub/PubSub.pm
@@ -0,0 +1,39 @@
+package PubNub::PubSub;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+PubNub::PubSub - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use PubNub::PubSub;
+
+=head1 DESCRIPTION
+
+PubNub::PubSub is
+
+=head1 AUTHOR
+
+Fayland Lam E<lt>fayland@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Fayland Lam
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
