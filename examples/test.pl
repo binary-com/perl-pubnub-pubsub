f0844d9e28483adb2bfb93b42ed5e42a817d5055
Fayland Lam <fayland@gmail.com>
2014-08-28 12:14:34 +0800

    Test large data and check time

--- examples/test.pl
+++ examples/test.pl
@@ -5,26 +5,44 @@ use warnings;
 use FindBin qw/$Bin/;
 use lib "$Bin/../lib";
 use PubNub::PubSub;
+use Time::HiRes qw/time/;
+
+srand();
+my $total_message = 50000 + int(rand(50000));
 
 #  s.send('GET /publish/pub-c-5afaf11d-aa91-4a40-b0d2-77961fb3a258/sub-c-0cd3a376-28ac-11e4-95a7-02ee2ddab7fe/0/HyperLogLogDemo1/0/"'+str(id[random.randrange(0, numID-1)])+'" HTTP/1.1\r\nHost: pubsub.pubnub.com\r\n\r\n')
+my $got_message = 0;
+my $start_time = time();
 my $pubnub = PubNub::PubSub->new(
     pub_key => 'pub-c-5afaf11d-aa91-4a40-b0d2-77961fb3a258',
     sub_key => 'sub-c-0cd3a376-28ac-11e4-95a7-02ee2ddab7fe',
     channel => 'HyperLogLogDemo1',
     callback => sub {
         my ($res, $req) = @_;
-        print "=" x 20 . "\n";
-        print "REQ: $req" . "\n";
-        print "=" x 20 . "\n";
-        print "RES: $res" . "\n";
-        print "=" x 20 . "\n";
+
+        # print "=" x 20 . "\n";
+        # print "REQ: $req" . "\n";
+        # print "=" x 20 . "\n";
+        # print "RES: $res" . "\n";
+        # print "=" x 20 . "\n";
+
+        $got_message++;
+        if ($got_message == $total_message or $got_message % 1000 == 0) {
+            my $duration = time() - $start_time;
+            print "$got_message Spent $duration.\n";
+        }
     }
 );
 
+print "Total sending $total_message\n";
 my @messages;
-foreach (1 .. 50) {
-    push @messages, "message" . int(rand(10000));
+foreach (1 .. $total_message) {
+    push @messages, "message" . int(rand(100000));
 }
 $pubnub->send(@messages);
+
+print "Total got $got_message VS $total_message\n";
+my $duration = time() - $start_time;
+print "Spent $duration.\n";
 
 1;
