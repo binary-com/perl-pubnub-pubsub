ac002f278764e3c58e0a5a4e999f36cdae55d8cc
Chris Travers <christravers@binary.com>
2014-11-13 06:11:02 +0000

    documentation and a minor change to Message.pm

--- lib/PubNub/PubSub/Message.pm
+++ lib/PubNub/PubSub/Message.pm
@@ -6,6 +6,33 @@ use Mojo::JSON qw(encode_json decode_json);
 use strict;
 use warnings;
 
+=head1 NAME
+
+PubNub::PubSub::Message - Message object for PubNub::PubSub
+
+=head1 SYNOPSIS
+
+This module is primarily used behind the scenes in PubNub::PubSub.  It is
+not intended to be used directly for users.  This being said, one can use it
+if you want to do your own URL management or otherwise interface with PubNub in 
+ways this distribution does not yet support.
+
+ my $message = PubNub::PubSub::Message->new(payload=> $datastructure);
+ my $json = $message->json;
+ my $payload = $message->payload;
+ my $queryhash = $message->query_params;
+
+=head1 METHODS
+
+=head2 new
+
+THis is the basic constructor.  Requires message or payload argument.  Message 
+is effectively an alias for payload.  Other arguments include ortt, meta, ear,
+and seqn, supported per the PubNub API.  These other arguments are converted
+to JSON in the query_params method below.
+
+=cut
+
 sub new {
     my $pkg  = shift;
     my %args = scalar @_ % 2 ? %{$_[0]} : @_;
@@ -15,20 +42,46 @@ sub new {
     return bless $self, $pkg;
 }
 
+=head2 payload
+
+Returns the message payload
+
+=cut
+
 sub payload {
     my $self = shift;
     return $self->{payload};
 }
 
+=head2 from_msg($json_string)
+
+Returns a message object with a payload from a json string.
+
+=cut
+
 sub from_msg {
     my ($self, $json) = @_;
-    return "$self"->new(payload => decode_json($json));
+    my $arrayref = decode_json($json);
+    return "$self"->new(payload => $arrayref->[0], timestamp => $arrayref->[1]);
 }
+
+=head2 json
+
+Returns the payload encoded in json via Mojo::JSON
+
+=cut
 
 sub json {
     my $self = shift;
     return encode_json($self->{payload});
 }
+
+=head2 query_params($mergehash)
+
+Returns a hash of query param properties (ortt, meta, ear, seqn), json-encoded,
+for use in constructing URI's for PubNub requests.
+
+=cut
 
 sub query_params {
     my $self = shift;
@@ -41,5 +94,12 @@ sub query_params {
             ();
     } qw(ortt meta ear seqn) };
 }
+
+=head2 LICENSE
+
+The copyright and license terms of this module are the same as those of the 
+PubNub::PubSub module with which it is distributed.
+
+=cut
 
 1;
